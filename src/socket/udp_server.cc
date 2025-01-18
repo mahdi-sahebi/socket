d0bc2a9b18062b988b7b9dad2b11dfc4533398b9
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-01-18 18:41:58 +0000

    [UPDATE]: UDP server APIs are decalred

--- src/socket/udp_server.cc
+++ src/socket/udp_server.cc
@@ -1,2 +1,47 @@
 #include "socket/udp_server.h"
 
+using namespace std;
+
+constexpr int INVALID_SOCKET = -1;
+
+
+UdpServer::UdpServer() :
+  socket_{INVALID_SOCKET}
+{
+}
+
+UdpServer::~UdpServer()
+{
+}
+
+void UdpServer::open()
+{
+
+}
+
+void UdpServer::close()
+{
+
+}
+
+bool UdpServer::isOpen()
+{
+  return false;
+}
+
+void UdpServer::bind(Port port)
+{
+}
+
+std::tuple<Data, Endpoint> UdpServer::read(uint32_t size, uint32_t timeoutUS)
+{
+  Endpoint senderEndpoint;
+  Data data(size);
+
+  return {data, senderEndpoint};
+}
+
+uint32_t UdpServer::write(Data data, Endpoint endpoint)
+{
+  return 0;
+}
