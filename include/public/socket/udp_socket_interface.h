6482e5059f44fd96254a4ac110f860d7a7d4ab46
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-01-18 17:43:13 +0000

    [UPDATE]: UDP exceptions are defined

--- include/public/socket/udp_socket_interface.h
+++ include/public/socket/udp_socket_interface.h
@@ -2,6 +2,7 @@
 #define UDP_SOCKET_INTERFACE_H_
 
 #include <tuple>
+#include <stdexcept>
 #include "socket_interface.h"
 
 
@@ -11,6 +12,27 @@ public:
   virtual std::tuple<Data, Endpoint> read(uint32_t size, uint32_t timeoutUS = 0) = 0;
   virtual uint32_t write(Data data, Endpoint endpoint) = 0;
 
+  class Exception
+  {
+  public:
+    class Port : public std::invalid_argument
+    {
+      public:
+        explicit Port(const std::string& message) : std::invalid_argument(message){}
+    };
+
+    class Open : public std::runtime_error
+    {
+    public:
+      explicit Open(const std::string& message) : std::runtime_error(message){}
+    };
+
+    class Close : public std::runtime_error
+    {
+    public:
+      explicit Close(const std::string& message) : std::runtime_error(message){}
+    };
+  };
 };
 
 
