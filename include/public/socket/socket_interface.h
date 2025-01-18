d1f96c2e67d24c5259fd3676817a20b3bf4b8063
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-01-18 17:33:42 +0000

    [UPDATE]: Socket interface is modified

--- include/public/socket/socket_interface.h
+++ include/public/socket/socket_interface.h
@@ -18,12 +18,12 @@ struct Endpoint
     ip{"0.0.0.0"},
     port{0}
   {
-
   }
 
-  Endpoint(IP ip, Port port){
-      this->ip = ip;
-      this->port = port;
+  Endpoint(IP ip, Port port)
+  {
+    this->ip = ip;
+    this->port = port;
   }
 
   IP ip;
@@ -31,24 +31,12 @@ struct Endpoint
 };
 
 // TODO(MN): Dervie from IO interface and handle files...
-class Socket
+class SocketInterface
 {
 public:
-    enum class Protocol
-    {
-        UDP = 0,
-        TCP
-    };
-
-    virtual ~Socket() = default;
-
-    virtual uint32_t send(const std::vector<char> data) = 0;
-    virtual std::vector<char> receive(uint32_t size, std::chrono::milliseconds timeout) = 0;
-
-    virtual uint32_t sendAsync(const std::vector<char> data, std::function<void>(void*)) = 0;
-    virtual std::vector<char> receiveAsync(uint32_t size, std::chrono::milliseconds timeout, std::function<void>(void*)) = 0;
-
-    virtual Protocol getProtocol() = 0;
+  virtual void open() = 0;
+  virtual void close() = 0;
+  virtual bool isOpen() = 0;
 };
 
 
