f07176f7fc802fdf44203f05e9bcece9a487eb80
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-01-30 17:30:07 +0330

    [TEST]: Test of read and write without endpoint is added

--- test/test_udp.cc
+++ test/test_udp.cc
@@ -134,7 +134,7 @@ TEST(Server, Write)
   }
 }
 
-TEST(Server, LargeReceive)
+TEST(Server, LargeReceive1)
 {
   const Port SERVER_PORT = 5000;
   const IP SERVER_IP = "127.0.0.1";
@@ -149,12 +149,12 @@ TEST(Server, LargeReceive)
         for (uint32_t testIndex = 0; testIndex < lengths.size(); testIndex++) {
           tuple<Data, Endpoint> result;
           do {
-            result = server.read(lengths[testIndex], 20);
+            result = server.read(lengths[testIndex], 200000);
           } while (std::get<0>(result).size() == 0);
 
           const auto& [data, endpoint] = result;
           EXPECT_EQ(data.size(), lengths[testIndex]);
-          EXPECT_TRUE(verifyData(data, testIndex));
+          EXPECT_TRUE(verifyData(data, testIndex));// TODO(MN): check endpoint
         }
 
         server.close();
@@ -169,6 +169,54 @@ TEST(Server, LargeReceive)
         for (uint32_t testIndex = 0; testIndex < lengths.size(); testIndex++) {
           const vector<char> buffer = generateData(lengths[testIndex], testIndex);
           const auto sentSize = client.write(buffer, Endpoint(SERVER_IP, SERVER_PORT));
+          EXPECT_EQ(sentSize, lengths[testIndex]);
+        }
+
+        client.close();
+    });
+
+    taskServer.get();
+    taskClient.get();
+  } catch (const exception& excp) {
+    std::cout << excp.what() << std::endl;
+    FAIL();
+  } catch (...) {
+    std::cout << "Unknown exception" << std::endl;
+  }
+}
+
+TEST(Server, LargeReceive2)
+{
+  const Port SERVER_PORT = 5000;
+  const IP SERVER_IP = "127.0.0.1";
+  const vector<uint32_t> lengths = {176, 865507, 867, 4705, 8, 98985};
+
+  try {
+    future<void> taskServer = async(launch::async, [SERVER_IP, lengths]() {
+      UdpServer server;
+      server.open();
+      server.bind(SERVER_PORT);
+
+      for (uint32_t testIndex = 0; testIndex < lengths.size(); testIndex++) {
+        Data data(lengths[testIndex], 0);
+        const auto receivedSize = server.read(data.data(), lengths[testIndex], 200000);
+        EXPECT_EQ(receivedSize, lengths[testIndex]);
+        EXPECT_EQ(data.size(), lengths[testIndex]);
+        EXPECT_TRUE(verifyData(data, testIndex));// TODO(MN): check endpoint
+      }
+
+      server.close();
+    });
+
+    future<void> taskClient = async(launch::async, [SERVER_IP, SERVER_PORT, lengths]() {
+        UdpClient client(Endpoint(SERVER_IP, SERVER_PORT));
+        client.open();
+
+        sleep_for(milliseconds(100));
+
+        for (uint32_t testIndex = 0; testIndex < lengths.size(); testIndex++) {
+          const vector<char> buffer = generateData(lengths[testIndex], testIndex);
+          const auto sentSize = client.write(buffer.data(), buffer.size());
           EXPECT_EQ(sentSize, lengths[testIndex]);
         }
 
